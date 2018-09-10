7f2834859404e42f48e7f6d28465121773cf7bc5
billyhinman712 <41489170+billyhinman712@users.noreply.github.com>
2018-09-10 08:41:23 -0700

    creatde seperate accounts

--- src/Account.js
+++ src/Account.js
@@ -5,15 +5,17 @@ export default class Account extends Component {
     super(props);
 
     this.state = {
-      balance: 0
+      balance: 0,
+      balance2: 0
     }
 
     this.handleDepositClick = this.handleDepositClick.bind(this)
+    this.handleWithdrawClick = this.handleWithdrawClick.bind(this)
   }
 
   handleDepositClick(e) {
     e.preventDefault();
-    if (isNaN(this.refs.amount.value)) {
+    if (isNaN(this.refs.amount.value)|| this.refs.amount.value < 0) {
       console.log("Not a number");
     }
     else {
@@ -26,6 +28,30 @@ export default class Account extends Component {
     }
   }
 
+  handleWithdrawClick(e) {
+    e.preventDefault();
+    if (isNaN(this.refs.amount.value)|| this.refs.amount.value < 0) {
+      console.log("Not a number");
+    }
+    else {
+      let amount = +this.refs.amount.value;
+      let newBalance = this.state.balance - amount;
+      if(newBalance < 0){
+        this.setState({
+        balance: 0
+        });
+      }
+      else{
+        let amount = +this.refs.amount.value;
+        let newBalance = this.state.balance - amount;
+        this.setState({
+          balance: newBalance
+        })
+      }
+      this.refs.amount.value = '';
+    }
+  }
+
   render() {
     let balanceClass = 'balance';
     if (this.state.balance === 0) {
@@ -33,11 +59,22 @@ export default class Account extends Component {
     }
 
     return (
-      <div className="account">
-        <h2>{this.props.name}</h2>
-        <div className={balanceClass}>${this.state.balance}</div>
-        <input type="text" placeholder="enter an amount" ref="amount" />
-        <input type="button" value="Deposit" onClick={this.handleDepositClick} />
+      <div>
+        <div className="account">
+          
+            <h2>{this.props.name1}</h2>
+            <div className={balanceClass}>${this.state.balance}</div>
+            <input type="text" placeholder="enter an amount" ref="amount" />
+            <input type="button" value="Deposit" onClick={this.handleDepositClick} />
+            <input type="button" value="Withdraw" onClick={this.handleWithdrawClick} />
+        </div>
+        <div className="account">
+            <h2>{this.props.name2}</h2>
+            <div className={balanceClass}>${this.state.balance}</div>
+            <input type="text" placeholder="enter an amount" ref="amount" />
+            <input type="button" value="Deposit" onClick={this.handleDepositClick} />
+            <input type="button" value="Withdraw" onClick={this.handleWithdrawClick} />
+        </div>
       </div>
     )
   }
--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <div id="logo"><img src={logo} alt="General Assembly logo" /></div>
           <div id="title">Bank of GA</div>
         </div>
-        <Account name="Checking" />
+        <Account name1="Checking" name2="Savings" />
         <div className="clear"></div>
       </div>
     );
